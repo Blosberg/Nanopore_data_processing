62116e593f5e2b080a49bf3d6873482acc1dc9f8
Blosberg <brendan.osberg@mdc-berlin.de>
2018-07-25 19:13:32 +0200

    collect sequences from reference genome for each modification site.

--- scripts/process_GR.R
+++ scripts/process_GR.R
@@ -8,32 +8,41 @@
 
 # ===== LOAD GRANGES OBJECTS PRODUCED BY SNAKEMAKE ======
 
+# @@@ TODO: make these arguments come from the command-line
+# e.g. [hard-coded for now]
+
 DIR_GR="/home/bosberg/projects/nanopore/pipeline_out/20180417_1233_polyA_RNA/06_GRobjects/"
 file_GR="ec9bd1f6f626d73709089ba0bfc63929581e9ede_GR.RData"
 load( file.path( DIR_GR, file_GR ) )
 
-# ======= LOAD PUTATIVE MODIFICATION LOCATIONS: =========
 put_path="/scratch/AG_Akalin/bosberg/nanopore/ref/Linder_2015_nature_supp/"
 CITS_filename="put_m6A_locs_CITS.csv"
 CIMS_filename="put_m6A_locs_CIMS.csv"
+
+
+hg19_ref <- readDNAStringSet("/scratch/AG_Akalin/refGenomes/hg19_canon/hg19_canon.fa")
+# ======= LOAD PUTATIVE MODIFICATION LOCATIONS: =========
 
 CITS_rawdat=read.csv( file.path( put_path, CITS_filename ), sep = "\t", header = TRUE )
 CIMS_rawdat=read.csv( file.path( put_path, CIMS_filename ), sep = "\t", header = TRUE )
 
 CIMS_put =  GRanges( seqnames = CIMS_rawdat$Chr ,
                      ranges   = IRanges ( start  = CIMS_rawdat$Start,
-                                     end    = CIMS_rawdat$End ),
+                                          end    = CIMS_rawdat$End ),
                      strand   = CIMS_rawdat$Strand
                     )
 CITS_put =  GRanges( seqnames = CITS_rawdat$Chr ,
                      ranges   = IRanges ( start  = CITS_rawdat$Start,
                                           end    = CITS_rawdat$End ),
                      strand   = CITS_rawdat$Strand
-)
+                    )
+
+CIMS_put = CIMS_put[ which( seqnames(CIMS_put) != "chrM") ]
+CITS_put = CITS_put[ which( seqnames(CITS_put) != "chrM") ]
 
 #  ================================================
+# write something specific for the m6A motif
 
-# start importing lines from "analysis.R" here.
 m6A_motif="GGACT"
 Nreads   = length(reads_GR)
 
@@ -50,6 +59,30 @@ reads_cits_putmod         = reads_GR[ reads_cits_putmod_indices ]
 reads_cits_putNonmod      = reads_GR[ reads_cits_nonmod_indices ]
 
 # ===================================================================
+# now generalize the above to arbitrary sequence:
+
+as.character(seqnames(CIMS_put[1]))
+# This is how to get the sequence from the reference of a single GRange
+get_refgen_seqs (  refgen = hg19_ref,
+                   ROI_GR = CIMS_put[2], 
+                   lead       = 2,
+                   trail      = 2,
+                   RNAstrand  = FALSE
+)
+  
+# so now do it in a batch.
+CITs_sequences = sapply( 1:length(CITS_put), function(x) get_refgen_seqs( refgen = hg19_ref, 
+                                                                ROI_GR = CITS_put[x], 
+                                                                lead=2, 
+                                                                trail=2, 
+                                                                RNAstrand=FALSE)  
+              )
+CIMs_sequences = sapply( 1:length(CIMS_put), function(x) get_refgen_seqs( refgen = hg19_ref, 
+                                                                          ROI_GR = CIMS_put[x], 
+                                                                          lead=2, 
+                                                                          trail=2, 
+                                                                          RNAstrand=FALSE)  
+)
 
 
 plot_compare_motif(seq = m6A_motif, 
--- scripts/process_GR_funcs.R
+++ scripts/process_GR_funcs.R
@@ -1,31 +1,129 @@
-plot_compare_motif <-  function( seq  = stop("seq  must be provided"),
-                                 Gro1 = stop("Gro1 must be provided"), 
-                                 Gro2 = stop("Gro2 must be provided"),
-                                 mincurrent = 50,
-                                 maxcurrent = 150
-                                 )
+seq_spec_compare <-  function( seq        = stop("seq  must be provided"),
+                               criterion  = "undefined",
+                               SOI_GR     = stop("sites of interest must be provided in GRanges format"), 
+                               control_GR = stop("control sites must be provided in GRanges format")
+                               )
 { 
-temp1 = Gro1[  Gro1$event_mean  > mincurrent ]
-temp1 = temp1[ temp1$event_mean < maxcurrent ]
 
-temp2 = Gro2[  Gro2$event_mean  > mincurrent ]
-temp2 = temp2[ temp2$event_mean < maxcurrent ]
+SOI_seq_GR     = SOI_GR[     SOI_GR$reference_kmer     == seq ]  # --- sites of interest with corresponding sequence.
+control_seq_GR = control_GR[ control_GR$reference_kmer == seq ]  # --- corresponding sequence in control locations (i.e. everywhere else.)
 
-temp1_subset= temp1[ temp1$reference_kmer == seq ]
-temp2_subset= temp2[ temp2$reference_kmer == seq ]
+result = list( "SOI_seq_GR" =  SOI_seq_GR, 
+               "control_seq_GR" = control_seq_GR, 
+               "seq"=seq,
+               "part_criterion" = criterion )
 
-# current_window = c( mincurrent, maxcurrent )
-par( mfrow = c(2,1) )
+return( result )
 
-hist( temp1_subset$event_mean, 
-      breaks = seq(from = mincurrent, to = maxcurrent, by = 1 ) , 
-      xlab = "current", 
-      main = m6A_motif )
-hist( temp2_subset$event_mean, 
-      breaks = seq(from = mincurrent, to = maxcurrent, by = 1 ) , 
-      xlab = "current", 
-      main = m6A_motif )
+}
 
-return(0)
+# ==================================================================
+
+get_refgen_seqs <-  function(  refgen     = stop("seq  must be provided"),
+                               ROI_GR     = stop("Region of interest must be provided in GRanges format"), 
+                               lead       = 0,
+                               trail      = 0,
+                               RNAstrand  = FALSE
+                               )
+{ 
+if ( as.character(strand(ROI_GR) ) == "+")
+  {
+
+  lo_end   <- start(ROI_GR) - lead;
+  hi_end   <- end(ROI_GR)   + trail;
+  flipseq = FALSE;
+
+  }else if( as.character(strand(ROI_GR) )  == "-") {
+
+  lo_end   <- start(ROI_GR) - trail;
+  hi_end   <- end(ROI_GR)   + lead;    # it will be flipped later.
+  flipseq = TRUE;
+
+  } else {
+  stop("undefined strand in get_refgen_seqs");
+  }
+
+result = eval ( 
+               parse( 
+                      text=paste0(
+                                   "refgen$",seqnames(ROI_GR),"[",as.character(lo_end),":",as.character(hi_end),"]" 
+                                  ) 
+                      ) 
+               )
+
+# ------------------------------ 
+if ( flipseq )
+  { # --- if the read is from the - strand, then reverse and compliment it
+  result = chartr("ATGC","TACG", stringi::stri_reverse( result ) )
+  }
+
+if( RNAstrand )
+  {
+  result = sub( "T", "U", result )
+  }
+
+return( result )
+
+}
+
+# ==================================================================
+
+
+plot_seq_spec_comparison  <- function( seq_spec_list,
+                                       mincurrent = 50,
+                                       maxcurrent = 150,
+                                       res = 1 )
+{
+  
+  temp1 = seq_spec_list$SOI_seq_GR[  seq_spec_list$SOI_seq_GR$event_mean  > mincurrent ]
+  temp1 = temp1[ temp1$event_mean < maxcurrent ]
+  
+  temp2 = seq_spec_list$control_seq_GR[  seq_spec_list$control_seq_GR$event_mean  > mincurrent ]
+  temp2 = temp2[ temp2$event_mean < maxcurrent ]
+  
+  # current_window = c( mincurrent, maxcurrent )
+  # par( mfrow = c(2,1) ) # === putting things into the same window now.
+  
+breakset = seq(from = mincurrent, 
+               to   = maxcurrent, 
+               by   = res ) 
+
+
+hist( temp2$event_mean,
+      freq = FALSE,
+      lty="blank",
+      col=rgb( 0.0, 0.0, 1, 0.5 ), 
+      main=paste("sequence=", 
+                 seq_spec_list$seq, 
+                 "\nputative vs. bulk alignments (",
+                 seq_spec_list$part_criterion,
+                 ")"),
+      breaks = breakset,
+      xlab = "current [pA]",
+      ylab = "prob" )
+
+hist( temp1$event_mean, 
+      freq = FALSE,                   # we want probabilities 
+      lty="blank",
+      col=rgb( 1, 0.0, 0.0, 0.5 ),  
+      breaks = breakset, 
+      add=T)
+# --------
+ # legend( 120, 0.08,
+ #         inset  = 0.2, 
+ #         legend = c("putative", "control"),
+ #         cex=0.8,
+ #         col=c( "red", "blue" ),
+ #         box.lwd=1
+ #        )
+
+ legend( maxcurrent - (0.4*(maxcurrent-mincurrent)), 0.07, # places a legend at the appropriate place c("Health","Defense"), # puts text in the legend 
+         lty=c(1,1), # gives the legend appropriate symbols (lines)
+         legend =c("putative", "control"),        
+         lwd=c(2.5,2.5),col=c("red","blue")) # g
+ 
+ 
+ 
+return(0);
 
 }
