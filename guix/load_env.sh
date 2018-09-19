baa80e1746234643ebfdaed46895159c44a158d5
Blosberg <brendan.osberg@mdc-berlin.de>
2018-09-19 11:38:58 +0200

    standardize environment generation

--- guix/load_env.sh
+++ guix/load_env.sh
@@ -1,4 +1,4 @@
-export GUIX_PROFILE=/home/bosberg/projects/nanopore/guix/.guix-profile
+export GUIX_PROFILE=${PWD}/.guix-profile
 
 export GUIX_LOCPATH=${GUIX_PROFILE}/lib/locale
 export GIT_SSL_CAINFO=$GUIX_PROFILE/etc/ssl/certs/ca-certificates.crt
