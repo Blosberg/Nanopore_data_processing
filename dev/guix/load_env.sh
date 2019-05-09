1d3819ad8cf7082b75884e1fe3f4cff1b7905ed5
Blosberg <Brendan.Osberg@mdc-berlin.de>
2019-05-09 11:19:06 +0200

    add git SSL info

--- dev/guix/load_env.sh
+++ dev/guix/load_env.sh
@@ -1,7 +1,11 @@
 export GUIX_PROFILE=${PWD}/.guix-profile
-
 export GUIX_LOCPATH=${GUIX_PROFILE}/lib/locale
-export GIT_SSL_CAINFO=$GUIX_PROFILE/etc/ssl/certs/ca-certificates.crt
+
+
+export SSL_CERT_DIR="${GUIX_PROFILE}/etc/ssl/certs"
+export SSL_CERT_FILE="${GUIX_PROFILE}/etc/ssl/certs/ca-certificates.crt"
+export GIT_SSL_CAINFO="${SSL_CERT_FILE}"
+
 source ${GUIX_PROFILE}/etc/profile   # now the environmental variables will be augmented (from the front)
 
 
