d741b8ee40da1fa4e119ec5a4b6cc1369412ddc9
Blosberg <brendan.osberg@mdc-berlin.de>
2019-05-21 10:22:20 +0200

    WIP: plotting currents scripts

--- scripts/08_GRdata_vis/Rfuncs_plot_currents_ROI.R
+++ scripts/08_GRdata_vis/Rfuncs_plot_currents_ROI.R
@@ -7,15 +7,15 @@ extract_sequence_from_npread  <- function( read_GR_in     = stop("read_GR_in mus
   # -------------------------------------------------------
   # read_input_GRL_splitby_startpos <- split( read_input_GR_called, start( read_input_GR_called) )
 
-  if( unique( as.character( strand( read_input_GR_called ) ) ) == "+"  ) 
-    { 
-    
+  if( unique( as.character( strand( read_input_GR_called ) ) ) == "+"  )
+    {
+
     read_ordered <- sort( read_input_GR_called, decreasing = FALSE )
-    
+
     } else if( unique( as.character( strand( read_input_GR_called ) ) ) == "-"  ){
 
     read_ordered <- sort( read_input_GR_called, decreasing = TRUE )
-    
+
   }  else{
     stop("ERROR extracting sequence; irregularity in strand information.")
   }
@@ -39,14 +39,14 @@ get_normalized_lt_hist <- function( GR_kmer_in      = stop("kmer-split event GR 
  breakset = seq( lt_histmin, lt_histmax, lt_histres)
 
  dat = log( GR_kmer_in$event_length)
- 
+
  if( min(dat) < lt_histmin || max(dat) > lt_histmax )
    {
    print("WARNING: data lies partially outside histogram range")
    dat[ dat < lt_histmin ] = lt_histmin+eps;
    dat[ dat > lt_histmax ] = lt_histmax-eps;
    }
- 
+
  result <- hist( log( GR_kmer_in$event_length),
                  breaks  = breakset,
                  plot    = FALSE
@@ -55,9 +55,65 @@ get_normalized_lt_hist <- function( GR_kmer_in      = stop("kmer-split event GR 
 }
 
 # ===============================================================
-get_list_of_read_olaps_with_ROIs <- function()
-{ ... }
-   
+# Get the loci that are adequately covered, and the reads that cover them
+filter_loci_coverage <- function( loci   = stop("Loci list must be provided"),
+                                  reads  = stop("readlist must be provided"),
+                                  mincov = 10)
+{
+overlaps_by_group = lapply ( names( loci ), function(x)
+                               findOverlaps(  loci[[x]],
+                               reads[[x]] )
+                               )
+names( overlaps_by_group ) <- names( loci )
+
+# collect a list of how many hits each loci accumulated:
+loci_coverage_by_group  = lapply( names( loci ),
+                                  function(group) collect_ROI_coverage ( group_loci     = loci[[group]],
+                                                                         group_overlaps = overlaps_by_group[[group]]  )
+)
+names( loci_coverage_by_group ) <- names(loci)
+
+
+# collect a list of which loci are above threshold coverage:
+covered_loci_list <- lapply( loci_coverage_by_group,
+                                  function(group) which( group >= mincov ) )
+
+
+# now filter those above threshold:
+loci_filtered_for_coverage <- lapply( names( loci ),
+                                      function(group) loci[[group]][ covered_loci_list[[group]] ] )
+names( loci_filtered_for_coverage ) <- names(loci)
+
+
+return( loci_filtered_for_coverage )
+}
+# ===============================================================
+# --- Filter olaps and reads:
+filter_olaps_and_reads <- function()
+{
+# find out which overlaps correspond to these loci:
+covered_overlaps_by_group = lapply( names( loci ),
+                                    function(group) overlaps_by_group[[group]][
+
+                                      which( ! is.na( match(  queryHits(  overlaps_by_group[[group]] ),
+                                                              covered_loci_list[[group]] )
+                                      )
+                                      ) ]
+)
+names( covered_overlaps_by_group ) <- names(loci)
+
+}
+# ===============================================================
+# --- collect coverage:
+collect_ROI_coverage <-  function( group_loci     = stop("group loci must be provided"),
+                                   group_overlaps = stop("group olaps must be provided"))
+{
+  result <- unlist( lapply( c(1:length( group_loci )),
+                            function(x) length( which( queryHits( group_overlaps ) == x ) )
+  )
+  )
+  return( result )
+}
 # ===============================================================
 # --- plot the current lines from all the reads of a sample over a given region:
 plot_samplesignal_over_ROI <-  function( SampleName = stop("Sample name must be provided"),
@@ -68,20 +124,20 @@ plot_samplesignal_over_ROI <-  function( SampleName = stop("Sample name must be 
                                          k=5,
                                          mincurrent = 50,
                                          maxcurrent = 150 )
-{ 
+{
   # for these inputs, we know that the reads overlap at least once on the ROI, but we don't know where
   Nreads = length( overlapping_reads )
-  
-  # get number of x positions in the plotting domain: 
+
+  # get number of x positions in the plotting domain:
   Nxpos     = end( ROI_in ) - start( ROI_in ) + 1;
   xvals     = c( start( ROI_in):end(ROI_in) )
   chr       = as.character( unique( seqnames( ROI_in ) ) )
 
-  
+
   # Get standard plot
-  # get_reference_strand # write a function here: 
+  # get_reference_strand # write a function here:
   # need to take reverse-compliment of ref genome for "-" strands
-  # Also need to transliterate "T" to "U" for RNA, 
+  # Also need to transliterate "T" to "U" for RNA,
   # use lapply to take ref values for mean and stand. dev.
   # ymean   = unlist ( lapply( Datin , function(x) c(x$event_level_mean, x$event_level_mean ) ) )
   # ystddev = unlist ( lapply( Datin , function(x) c(x$event_stdv,       x$event_stdv ) ) )
@@ -91,32 +147,32 @@ plot_samplesignal_over_ROI <-  function( SampleName = stop("Sample name must be 
          col="black",
          type="l",
          lty = "dashed",
-         lwd=2,      
-         xlim = c( start(ROI_in), 
+         lwd=2,
+         xlim = c( start(ROI_in),
                    end(ROI_in) ),
-         
+
          main= paste(  SampleName, "\n",
-                       chr, 
-                       ":", 
+                       chr,
+                       ":",
                        as.character( start(ROI_in) ),
-                       "-", 
+                       "-",
                        as.character( end(ROI_in)  ) ),
          xlab = "reference position",
          xaxt="n",
          ylab = "current [pA]",
-         ylim = c( mincurrent, 
+         ylim = c( mincurrent,
                    maxcurrent )
       )
-  
+
   # add reference sequence, complement, and directionality.
   add_sequence_tick_marks( refgen = refgen,
                            Locus  = ROI_in )
-  
+
   # plot a transparent polygon around the +/-1 std.dev region of the standard (expected) current values.
-  polygon( c( xvals, rev(xvals) ), 
-           c(  (ymean-ystddev), 
+  polygon( c( xvals, rev(xvals) ),
+           c(  (ymean-ystddev),
                rev(ymean+ystddev) ),
-           
+
            col  = rgb(0, 0, 0, 0.1),
            lty  = "blank"
           )
@@ -124,7 +180,7 @@ plot_samplesignal_over_ROI <-  function( SampleName = stop("Sample name must be 
   #========  NOW PLOT THE READS THEMSELVES =========
   # Get the particular position within each read where it overlaps with the ROI
 
-  # populate a matrix of current values at each position.  
+  # populate a matrix of current values at each position.
   plotdat <- matrix( NA, Nreads, Nxpos )
   for (i in c(1:Nreads) )
     {
@@ -132,7 +188,7 @@ plot_samplesignal_over_ROI <-  function( SampleName = stop("Sample name must be 
     plotdat[ i, xivals ] <-  overlapping_reads[[i]]$event_level_mean;
     # positions in each read without current data are left "NA"
   # plot the rows of this matrix for each read
-  lines( xvals, 
+  lines( xvals,
          plotdat[i, ],
          col    =rgb(0,0,1,0.5),
          type   = "l",
@@ -156,40 +212,40 @@ add_sequence_tick_marks   <- function(  refgen        = stop("Reference genome m
   xvals             = c( start( Locus):end(Locus) )
   chrom_seq         = eval( parse( text=paste0( "refgen$", as.character( unique( seqnames(Locus))) ) ) )
   seqchars_plus     = chrom_seq[ xvals ]
-  seqchars_neg      = chartr("ACGT", "TGCA", seqchars_plus ) 
-  
+  seqchars_neg      = chartr("ACGT", "TGCA", seqchars_plus )
+
   # Now label give plus/neg strand appropriate emphasis:
     if ( strand( Locus ) == "+" )
   { x0 = start(  Locus )
     x1 = end(Locus)
-    Col_plusstrand = Col_sense 
+    Col_plusstrand = Col_sense
     Col_negstrand  = Col_antisense  } else if( strand(Locus) =="-" ){
-      
+
     x0 = end(   Locus )
-    x1 = start( Locus ) 
-    Col_plusstrand = Col_antisense 
+    x1 = start( Locus )
+    Col_plusstrand = Col_antisense
     Col_negstrand  = Col_sense
-    
+
     } else{ stop("ill-defined strand.")}
 
   # Now label the sequence in place:
   for ( xi  in c(1:length(xvals)) )
-  { axis(1, 
+  { axis(1,
          at = xvals[xi],
          seqchars_plus[ xi ],
          padj = 1,
          col.axis = Col_plusstrand
-         ) 
-     axis(1, 
+         )
+     axis(1,
          at = xvals[xi],
          seqchars_neg[ xi ],
          padj = -0.5,
          col.axis = Col_negstrand
-         ) 
+         )
   }
-  
+
   # Finally, draw an arrow indicating direction of transcription.
-  arrows( x0, mincurrent, x1, mincurrent, 
+  arrows( x0, mincurrent, x1, mincurrent,
           length = 0.25, angle = 20,
           code = 2, col = par("fg"), lty = par("lty"),
           lwd = 3 )
--- scripts/08_GRdata_vis/Rmain_plot_currents_ROI.R
+++ scripts/08_GRdata_vis/Rmain_plot_currents_ROI.R
@@ -1,35 +1,87 @@
-# modification_analysis.R
-# ---Driver script for mod analysis.
+# Plot current from reads along RsoI:
 
-library(Gviz)
-library(GenomicRanges)
-library(BSgenome.Hsapiens.UCSC.hg19 )
+## Collect arguments
+args <- commandArgs(TRUE)
 
-assembly   = "hg19"
-mincurrent = 50
-maxcurrent = 250
+## Default setting when no arguments passed
+if(length(args) < 1) {
+  args <- c("--help")
+}
 
-SampleName = "HEK293_unmod"
+## Help section
+if("--help" %in% args) {
+  cat("
+      Render to report
 
-Refgenome_path <- "/scratch/AG_Akalin/refGenomes/hg19_canon/hg19_canon.fa"
-hg19_ref       <-  readDNAStringSet( Refgenome_path )
-hg19_strack    <-  SequenceTrack(hg19_ref)
+      Arguments:
+      pathin_reads         --path to Granges List structure with reads aligned to reference genome,
+      pathin_RsoI          --path to Granges list of regions of interest in the current study,
+      pathout_plot         --where should we send the plots
+      assembly             --genome
+      logFile              -- self-explanatory
 
-HEK293_MM2po_reads <- readRDS( "/fast/AG_Akalin/bosberg/nanopore/pipeline_output/20180417_1233_HEK293_polyA_RNA/07_GRprocessing/HEK_untreated_read_ROIolap_MihaM_2pO.rds" )
-Miha_2po_sites <- readRDS("/fast/AG_Akalin/bosberg/nanopore/ref/Regions_of_interest/MihaM_2po_Ill.rds")
+      Example:
+      $Rscript ./Rmain_olap_reads_w_RsoI.R --arg1=1 --arg2='output.txt' --arg3=TRUE \n\n")
+
+  q(save="no")
+}
+
+## Parse arguments (we expect the form --arg=value)
+parseArgs <- function(x) strsplit(sub("^--", "", x), "=")
+argsDF    <- as.data.frame(do.call("rbind", parseArgs(args)))
+argsL     <- as.list(as.character(argsDF$V2))
+
+names(argsL) <- argsDF$V1
+BiocManager::install("Biostrings")
+BiocManager::install("BSgenome.Hsapiens.UCSC.hg19")
+suppressPackageStartupMessages( library(GenomicRanges) )
+suppressPackageStartupMessages( library(Biostrings) )
+
+##==============================================================
 
 
-Ni = 63 # arbitrarily chosen: just need to plot something
-Locus <- Miha_2po_sites$Region_groups$Ill_2pO[Ni]
-start( Locus  ) <- 26227566
-end(   Locus  ) <- 26227586 
+# ======= DEBUGGING: DELETE THIS: ========
+argsL=list(
+"pathin_reads"   = "/Users/lana/Desktop/train_dat/olap_files/HEK_untreated_read_ROIolap_m6A_put.rds",  # path to Granges List structure with reads aligned to reference genome
+"pathin_RsoI"    = "/Users/lana/Desktop/train_dat/putmod_locs/m6A_putlocs_Linder.rds",                 # path to Granges list of regions of interest in the current study
+"pathin_refGen"  = "/Users/lana/Desktop/train_dat/refGenome/hg19_canon.fa.gz",                         # path to Granges list of regions of interest in the current study
+"pathout_plot"   = "/Users/lana/Desktop/train_dat/test_out/localtest.out",                             # where should we send the plots
+"logFile"        = "/Users/lana/Desktop/train_dat/test_out/localtest.log",                             # self-explanatory
+"assembly"       = "hg19",
+"sampleName"     = "HEK293_untreated",
+"mincov_in"      = 10
+)
+suppressPackageStartupMessages( library(BSgenome.Hsapiens.UCSC.hg19 ) )
+ref_Genome    <- BSgenome.Hsapiens.UCSC.hg19
+group="CIMS"
+
+# ======= DEBUGGING: DOWN TO HERE: ========
+
+# ========================================
+# Read in inputs:
+ref_Genome    <- readDNAStringSet( argsL$pathin_refGen )
+readdat       <- readRDS( argsL$pathin_reads )
+putloci       <- readRDS( argsL$pathin_RsoI  )
+
+# ========================================
+# Read in inputs:
+
+# TODO: add sanity check that reference names agree.
+
+read_RoI_filtered_for_coverage <- filter_loci_coverage (  loci   = putloci$Region_groups,
+                                                          reads  = readdat$aligned_reads,
+                                                          mincov = mincov_in
+                                                        )
+readdat$aligned_reads
+
+
 chr_Locus = as.character( seqnames( Locus) )
 strand_Locus = as.character( unique( strand( read) ) )
 
-plotframe = GRanges( seqnames = chr_Locus, 
+plotframe = GRanges( seqnames = chr_Locus,
                      strand   = strand_Locus,
-                     ranges   = IRanges( c( start(Locus):end(Locus) ) , 
-                                         end =  c( start(Locus):end(Locus) )  )   
+                     ranges   = IRanges( c( start(Locus):end(Locus) ) ,
+                                         end =  c( start(Locus):end(Locus) )  )
                      )
 
 # take reads that were aligned and putative modification sites:
@@ -47,17 +99,17 @@ olaps = findOverlaps( plotframe, read )
 plot( start(read[ subjectHits(olaps)] ),
                 read[ subjectHits( olaps )  ]$event_mean,
       type = "l",
-      xlim = c( start(Locus), 
+      xlim = c( start(Locus),
                 end(Locus)),
       main= paste(  SampleName, "\n",
-                    as.character( unique( seqnames(Locus) ) ), 
-                    ":", 
+                    as.character( unique( seqnames(Locus) ) ),
+                    ":",
                     as.character(start(Locus) ),
-                    "-", 
+                    "-",
                     as.character( end(Locus)  ) ),
       xlab = paste( "+ strand sequence." ),
       xaxt="n",
-      
+
       ylim = c(mincurrent, maxcurrent),
       ylab = "current[pA]",
       col  = rgb( 0, 0, 1, 0.5 )
@@ -65,12 +117,12 @@ plot( start(read[ subjectHits(olaps)] ),
 
 # prepare the sequence tick-mark labels:
 xcoords = seq( start(Locus), end(Locus), 1 )
-xlabels = unlist( strsplit( as.character( hg19_ref$chr1[ start(Locus): end(Locus) ] ), 
+xlabels = unlist( strsplit( as.character( ref_Genome$chr1[ start(Locus): end(Locus) ] ),
                    split = "" ) )
 
 # apply the sequence tick-mark labels:
 for ( i  in c(1:length(xcoords)) )
-  { axis(1, 
+  { axis(1,
        at = xcoords[i],
        xlabels[i]
        ) }
